33493e06ed8048d44815a545c4f278fb19f7bb11
Daria V <voronovadaria@gmail.com>
2021-07-14 21:49:46 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #177733] fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,13 +56,12 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        boolean rsl = false;
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            rsl = true;
         }
         return rsl;
     }
