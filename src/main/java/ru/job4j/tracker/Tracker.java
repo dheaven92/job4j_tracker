e91f907dbb0af57d53a8686391c585023ff699f4
Daria V <voronovadaria@gmail.com>
2021-07-14 21:20:17 +0300

    5. Tracker - хранилище [#396 #176937] fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,13 +32,12 @@ public class Tracker {
 
     public Item[] findByName(String key) {
         Item[] result = new Item[size];
-        int size = 0;
-        for (Item item : items) {
-            if (item != null && item.getName() != null
-                    && item.getName().equals(key)) {
-                result[size++] = item;
+        int resultIndex = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[resultIndex++] = items[i];
             }
         }
-        return Arrays.copyOf(result, size);
+        return Arrays.copyOf(result, resultIndex);
     }
 }
