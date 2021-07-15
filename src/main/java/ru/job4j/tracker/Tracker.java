598b5db9c526221d6c7289f69740c2d89d075102
Daria V <voronovadaria@gmail.com>
2021-07-15 18:39:18 +0300

    minor fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -44,17 +44,6 @@ public class Tracker {
         return rsl;
     }
 
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
-    }
-
     public boolean delete(int id) {
         int index = indexOf(id);
         boolean rsl = index != -1;
@@ -62,6 +51,17 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
         }
         return rsl;
     }
