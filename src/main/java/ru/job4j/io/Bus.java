5616682cdf78cd76a1aaf7a52a1d83fcda9e466d
Daria V <voronovadaria@gmail.com>
2021-07-18 11:35:31 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/io/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void setPassengers(int passengers) {
+
+    }
+
+    @Override
+    public double setFuel(int gallons) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/io/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.io;
+
+public interface Transport {
+
+    void drive();
+
+    void setPassengers(int passengers);
+
+    double setFuel(int gallons);
+}
