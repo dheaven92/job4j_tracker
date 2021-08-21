1b635186f29f0efbfcdad32916263e48cd2b96c0
Daria V <voronovadaria@gmail.com>
2021-08-21 10:12:41 +0300

    4. Преобразование List в Map. [#110226]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,5 +11,14 @@ public class School {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Student> map(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        s -> s,
+                        (s1, s2) -> s1
+                ));
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -3,7 +3,9 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -63,6 +65,41 @@ public class SchoolTest {
         expected.add(new Student(10, "Surname1"));
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenMapEqual() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(40, "Surname4")
+        );
+        School sc = new School();
+        Map<String, Student> expected = new HashMap<>(Map.of(
+                "Surname1", new Student(10, "Surname1"),
+                "Surname3", new Student(30, "Surname3"),
+                "Surname4", new Student(40, "Surname4")
+        ));
+        Map<String, Student> rsl = sc.map(students);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenMapDuplicates() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(12, "Surname1"),
+                new Student(40, "Surname4")
+        );
+        School sc = new School();
+        Map<String, Student> expected = new HashMap<>(Map.of(
+                "Surname1", new Student(10, "Surname1"),
+                "Surname3", new Student(30, "Surname3"),
+                "Surname4", new Student(40, "Surname4")
+        ));
+        Map<String, Student> rsl = sc.map(students);
         assertThat(rsl, is(expected));
     }
 }
