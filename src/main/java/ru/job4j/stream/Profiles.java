7b2f73fdba8cae2bc2bec3c42fc34ebb31d0c6bd
Daria V <voronovadaria@gmail.com>
2021-08-21 09:51:32 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,6 +1,6 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +9,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ProfilesTest {
 
@@ -69,5 +70,63 @@ public class ProfilesTest {
         );
         List<Address> actualAddresses = new Profiles().collect(profiles);
         assertNotEquals(expectedAddresses, actualAddresses);
+    }
+
+    @Test
+    public void whenDuplicates() {
+        List<Profile> profiles = new ArrayList<>(List.of(
+                new Profile(new Address(
+                        "Yaroslavl",
+                        "Lenina",
+                        10,
+                        11
+                )),
+                new Profile(new Address(
+                        "Moscow",
+                        "Lenina",
+                        8,
+                        34
+                )),
+                new Profile(new Address(
+                        "Moscow",
+                        "Lenina",
+                        8,
+                        34
+                )),
+                new Profile(new Address(
+                        "St. Petersburg",
+                        "Krasnaya",
+                        10,
+                        5
+                )),
+                new Profile(new Address(
+                        "Moscow",
+                        "Lenina",
+                        8,
+                        34
+                ))
+        ));
+        List<Address> expectedAddresses = List.of(
+                new Address(
+                        "Moscow",
+                        "Lenina",
+                        8,
+                        34
+                ),
+                new Address(
+                        "St. Petersburg",
+                        "Krasnaya",
+                        10,
+                        5
+                ),
+                new Address(
+                        "Yaroslavl",
+                        "Lenina",
+                        10,
+                        11
+                )
+        );
+        List<Address> actualAddresses = new Profiles().collect(profiles);
+        assertEquals(expectedAddresses, actualAddresses);
     }
 }
