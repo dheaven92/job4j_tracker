5affdf4476854c7d230a1193437bfcd2d661b762
Daria V <voronovadaria@gmail.com>
2021-08-23 20:30:23 +0300

    1.4.2. Stream API task part 2

---
+++ src/main/java/ru/job4j/stream/extra/CharsMethod.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream.extra;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CharsMethod {
+
+    public static List<Character> symbols(String string) {
+        return string.chars()
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.toList());
+    }
+}
