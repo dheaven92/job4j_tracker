146e3fefef30ae391e75e6aacb5eeff1cd1d177b
Daria V <voronovadaria@gmail.com>
2021-08-24 20:33:57 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/departments/DepartmentsTest.java
@@ -10,24 +10,24 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenMissedMore() {
-        List<String> input = Arrays.asList("k1/sk1/ssk1", "k2/sk2");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k2", "k2/sk2");
+        List<String> input = List.of("k1/sk1/ssk1", "k2/sk2");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1", "k2", "k2/sk2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -45,7 +45,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -73,7 +73,7 @@ public class DepartmentsTest {
                 "K1/SK1/SSK2",
                 "K1/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
